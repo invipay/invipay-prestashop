2ba39605cae8d2f972efc552d307a6c41f7d9a87
Kuba Pilecki <k.pilecki@gmail.com>
2016-02-15 17:15:59 +0100

    Better package script

--- package.sh
+++ package.sh
@@ -1,3 +1,12 @@
 #!/bin/bash
-tar -zchvf Presta_Invipay_Paygate.tgz --exclude='.DS_Store' invipaypaygate
-zip -r invipaypaygate.zip invipaypaygate -x *.DS_Store*
+
+TARGET_DIR=$1
+
+if [ -z "$1" ]; then TARGET_DIR="."; fi
+
+TARGET_FILE="$TARGET_DIR/Prestashop_Invipay_Paygate.zip"
+
+if [ -f "$TARGET_FILE" ]; then rm "$TARGET_FILE"; fi
+
+echo "Packaging PrestaShop plugin into: $TARGET_FILE"
+zip -r "$TARGET_FILE" invipaypaygate -x *.DS_Store* -x *.git* -x *.gitmodules*
